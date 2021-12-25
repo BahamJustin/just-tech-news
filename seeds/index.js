0959ce6d0372355fa8198d17504a6be1f7da08ea
BahamJustin <bahamjustin@outlook.com>
2021-12-25 17:05:16 -0600

    working fullstack application

--- seeds/index.js
+++ seeds/index.js
@@ -1,24 +1,25 @@
-const seedUsers = require('./user-seeds');
-const seedPosts = require('./post-seeds');
-const seedComments = require('./comment-seeds');
-const seedVotes = require('./vote-seeds');
+const seedUsers = require("./user-seeds");
+const seedPosts = require("./post-seeds");
+const seedComments = require("./comment-seeds");
+const seedVotes = require("./vote-seeds");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  console.log('--------------');
+  console.log("Database Created");
+
   await seedUsers();
-  console.log('--------------');
+  console.log("Users Seeded");
 
   await seedPosts();
-  console.log('--------------');
+  console.log("Posts Seeded");
 
   await seedComments();
-  console.log('--------------');
+  console.log("Comments Seeded");
 
   await seedVotes();
-  console.log('--------------');
+  console.log("Votes Seeded");
 
   process.exit(0);
 };
